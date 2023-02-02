95cbaedf512d366a05904d4bd794b851bbe51bee
vferat <victor.ferat@live.fr>
2023-02-02 20:12:46 +0100

    add analysis

--- check_impedance.py
+++ check_impedance.py
@@ -1,7 +1,7 @@
 import asyncio
 from idun_guardian_client import GuardianClient
 
-IMPEDANCE_DURATION = 5  # duration of impedance measurement in seconds
+IMPEDANCE_DURATION = 60  # duration of impedance measurement in seconds
 MAINS_FREQUENCY_60Hz = False
 # mains frequency in Hz (50 or 60), for Europe 50Hz, for US 60Hz
 
